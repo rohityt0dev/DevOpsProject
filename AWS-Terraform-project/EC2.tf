000900b23211e87c43dcb0a724ad9738338fd16f
rohityt0dev <rohityt0@gmail.com>
2026-04-29 01:53:12 +0530

    commit 3
    Added resources for VPC, subnet, internet gateway, route table, and security group to set up a web server infrastructure.

--- AWS-Terraform-project/EC2.tf
+++ AWS-Terraform-project/EC2.tf
@@ -1,23 +1,69 @@
-resource "aws_instance" "web_server" {
-  ami           = var.ami_id # Amazon Linux (Mumbai)
-  instance_type = var.instance_type
-
-  key_name = var.key_name
-
-  subnet_id              = aws_subnet.public_subnet.id
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-
-  associate_public_ip_address = true
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum install -y httpd
-              systemctl start httpd
-              systemctl enable httpd
-              echo "Hello from Terraform" > /var/www/html/index.html
-              EOF
+#  Create VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block = var.vpc_cidr
 
   tags = {
-    Name = "web-server"
+    Name = "Terraform-vpc"
+  }
+}
+
+# Create Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = var.subnet_cidr
+
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "Terraform-igw"
+  }
+}
+
+# Create Routr table and associate with subnet
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rt_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
