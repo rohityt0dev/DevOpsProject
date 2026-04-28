8d9466dcc8dda08f67adf221d02179faa323ff8c
rohityt0dev <rohityt0@gmail.com>
2026-04-29 01:55:01 +0530

    commit 3

--- AWS-Terraform-project/variables.tf
+++ AWS-Terraform-project/variables.tf
@@ -25,13 +25,13 @@ variable "availability_zone" {
 # EC2 Instance Type
 variable "instance_type" {
   description = "EC2 instance type"
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 # AMI ID (Amazon Linux - Mumbai)
 variable "ami_id" {
   description = "AMI ID for EC2 instance"
-  default     = "ami-0f5ee92e2d63afc18"
+  default     = "ami-0e12ffc2dd465f6e4"
 }
 
 # Key Pair Name (IMPORTANT for SSH)
@@ -39,4 +39,5 @@ variable "key_name" {
   description = "EC2 Key Pair name"
   default     = "key"
 }
+
 
